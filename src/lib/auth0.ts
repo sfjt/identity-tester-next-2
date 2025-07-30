dce27b91cbf4cba8d01d5c24d8ce15fd13dce207
Sho Fujita <sho.fujita@okta.com>
2025-07-30 15:29:21 +0900

    Database session

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,20 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server"
+import { Redis } from "@upstash/redis"
+import { SessionData } from "@auth0/nextjs-auth0/types"
 
-export const auth0 = new Auth0Client()
+const redis = Redis.fromEnv()
+
+export const auth0 = new Auth0Client({
+  sessionStore: {
+    async get(id) {
+      return redis.get<SessionData>(id)
+    },
+    async set(id, sessionData) {
+      const { expiresAt } = sessionData.tokenSet
+      await redis.set<SessionData>(id, sessionData, { exat: expiresAt })
+    },
+    async delete(id) {
+      await redis.del(id)
+    },
+  }
+})
