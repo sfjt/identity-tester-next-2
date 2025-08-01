6cc72d6a74b472189093c041a021feaff5324d36
Sho Fujita <sho.fujita@okta.com>
2025-08-01 13:37:39 +0900

    Back-channel logout

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -2,19 +2,78 @@ import { Auth0Client } from "@auth0/nextjs-auth0/server"
 import { Redis } from "@upstash/redis"
 import { SessionData } from "@auth0/nextjs-auth0/types"
 
+interface IdsArray {
+  ids: Array<string>
+  expiresAt: number
+}
+
 const redis = Redis.fromEnv()
 
 export const auth0 = new Auth0Client({
   sessionStore: {
     async get(id) {
-      return redis.get<SessionData>(id)
+      try{
+        return await redis.get<SessionData>(id)
+      } catch (err) {
+        console.error(err)
+        return null
+      }
     },
     async set(id, sessionData) {
       const { expiresAt } = sessionData.tokenSet
-      await redis.set<SessionData>(id, sessionData, { exat: expiresAt })
+      const { sid } = sessionData.internal
+      const { sub } = sessionData.user
+
+      const tasks: Array<Promise<IdsArray | SessionData | "OK" | null>> = []
+
+      const idsBySid = await redis.get<IdsArray>(`sid:${sid}`) || {ids: [], expiresAt: 0}
+      idsBySid.ids.push(id)
+      const sidMaxExp = idsBySid.expiresAt > expiresAt ? idsBySid.expiresAt : expiresAt
+      tasks.push(redis.set<IdsArray>(`sid:${id}`, { ids: [...new Set(idsBySid.ids)], expiresAt: sidMaxExp }, { exat: sidMaxExp }))
+
+      const idsBySub = await redis.get<IdsArray>(`sub:${sub}`) || {ids: [], expiresAt: 0}
+      idsBySub.ids.push(id)
+      const subMaxExp = idsBySub.expiresAt > expiresAt ? idsBySub.expiresAt : expiresAt
+      tasks.push(redis.set<IdsArray>(`sub:${sub}`, { ids: [...new Set(idsBySub.ids)], expiresAt: subMaxExp }, { exat: subMaxExp }))
+
+      tasks.push(redis.set<SessionData>(id, sessionData, { exat: expiresAt }))
+
+      const result = await Promise.allSettled(tasks)
+      result.filter(data => data.status === "rejected").forEach( data => {
+        console.error(`${data.status}: ${data.reason}`)
+      })
     },
     async delete(id) {
-      await redis.del(id)
+      try {
+        await redis.del(id)
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    async deleteByLogoutToken({ sid, sub }) {
+      const tasks: Array<Promise<number>> = []
+      if (sid) {
+        const idsBySid = await redis.get<IdsArray>(`sid:${sid}`)
+        if (idsBySid) {
+          idsBySid.ids.forEach(id => {
+            tasks.push(redis.del(id))
+          })
+        }
+      }
+
+      if (sub) {
+        const idsBySub = await redis.get<IdsArray>(`sub:${sub}`)
+        if (idsBySub) {
+          idsBySub.ids.forEach(id => {
+            tasks.push(redis.del(id))
+          })
+        }
+      }
+
+      const result = await Promise.allSettled(tasks)
+      result.filter(data => data.status === "rejected").forEach( data => {
+        console.error(`${data.status}: ${data.reason}`)
+      })
     },
   },
 })
