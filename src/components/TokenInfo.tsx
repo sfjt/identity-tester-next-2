f92fd4d198583bca62c49937d8e2a5edee146280
Sho Fujita <sho.foujita@gmail.com>
2026-02-04 11:45:41 +0900

    Code formatting

--- src/components/TokenInfo.tsx
+++ src/components/TokenInfo.tsx
@@ -7,7 +7,11 @@ export default function TokenInfo(props: { jwt: string | undefined }) {
     <>
       <p className="bg-gray-200 p-3 rounded-sm font-mono text-xs break-all">{jwt}</p>
       <p className="text-right">
-        <a href={`https://jwt.io/#token=${jwt}`} target="_blank" className="text-primary no-underline text-sm break-all hover:underline">
+        <a
+          href={`https://jwt.io/#token=${jwt}`}
+          target="_blank"
+          className="text-primary no-underline text-sm break-all hover:underline"
+        >
           jwt.io
         </a>
       </p>
