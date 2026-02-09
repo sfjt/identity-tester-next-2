9c03ad846e529c908f3c1a53b75d8c2ad904b590
Sho Fujita <sho.foujita@gmail.com>
2026-02-09 16:45:38 +0900

    Passkey testing page

---
+++ src/app/passkey/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import Script from "next/script"
+
+export default function Page() {
+  return (
+    <main className="font-['Roboto',sans-serif] bg-white p-8 rounded-lg shadow-md">
+      <h2 className="text-gray-800 text-2xl font-bold">Passkey Testing</h2>
+      <section className="mb-8">
+        <h3 className="text-gray-800 mb-4 text-lg font-bold">Client Capabilities</h3>
+        <pre id="capabilities"></pre>
+        <Script>
+          {`
+            ;(async function() {
+              const capabilities = await window.PublicKeyCredential.getClientCapabilities()
+              console.log(capabilities)
+              document.getElementById("capabilities").appendChild(document.createTextNode(JSON.stringify(capabilities, null, 2)))
+            })()
+          `}
+        </Script>
+      </section>
+    </main>
+  )
+}
