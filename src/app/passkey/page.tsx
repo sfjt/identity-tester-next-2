d6d9836bd7794273b670d02b4b8c9576a871348c
Sho Fujita <sho.foujita@gmail.com>
2026-02-09 17:00:00 +0900

    Passkey testing page

--- src/app/passkey/page.tsx
+++ src/app/passkey/page.tsx
@@ -7,14 +7,20 @@ export default function Page() {
     <main className="font-['Roboto',sans-serif] bg-white p-8 rounded-lg shadow-md">
       <h2 className="text-gray-800 text-2xl font-bold">Passkey Testing</h2>
       <section className="mb-8">
-        <h3 className="text-gray-800 mb-4 text-lg font-bold">Client Capabilities</h3>
+        <h3 className="text-gray-800 mb-4 text-lg font-bold">getClientCapabilities</h3>
         <pre id="capabilities"></pre>
+        <h3 className="text-gray-800 mb-4 text-lg font-bold">isUserVerifyingPlatformAuthenticatorAvailable</h3>
+        <pre id="platform"></pre>
         <Script>
           {`
             ;(async function() {
               const capabilities = await window.PublicKeyCredential.getClientCapabilities()
-              console.log(capabilities)
+              console.log("capabilities", capabilities)
               document.getElementById("capabilities").appendChild(document.createTextNode(JSON.stringify(capabilities, null, 2)))
+              
+              const isPlatformAuthenticatorAvailable = await window.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable()
+              console.log("isPlatformAuthenticatorAvailable", isPlatformAuthenticatorAvailable)
+              document.getElementById("platform").appendChild(document.createTextNode(isPlatformAuthenticatorAvailable.toString()))
             })()
           `}
         </Script>
