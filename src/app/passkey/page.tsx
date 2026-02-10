3fd9551a08ef55265821cda4bd757b66a1b612b9
Sho Fujita <sho.foujita@gmail.com>
2026-02-10 10:32:40 +0900

    Styling

--- src/app/passkey/page.tsx
+++ src/app/passkey/page.tsx
@@ -7,20 +7,24 @@ export default function Page() {
     <main className="font-['Roboto',sans-serif] bg-white p-8 rounded-lg shadow-md">
       <h2 className="text-gray-800 text-2xl font-bold">Passkey Testing</h2>
       <section className="mb-8">
-        <h3 className="text-gray-800 mb-4 text-lg font-bold">getClientCapabilities</h3>
-        <pre id="capabilities"></pre>
-        <h3 className="text-gray-800 mb-4 text-lg font-bold">isUserVerifyingPlatformAuthenticatorAvailable</h3>
-        <pre id="platform"></pre>
+        <h3 className="text-gray-800 mb-4 text-lg font-bold">Client Capabilities</h3>
+        <pre id="client-capabilities"></pre>
+      </section>
+      <section className="mb-8">
+        <h3 className="text-gray-800 mb-4 text-lg font-bold">User Verifying Platform Authenticator</h3>
+        <pre id="is-user-verifying-platform-authenticator-available"></pre>
         <Script>
           {`
             ;(async function() {
               const capabilities = await window.PublicKeyCredential.getClientCapabilities()
               console.log("capabilities", capabilities)
-              document.getElementById("capabilities").appendChild(document.createTextNode(JSON.stringify(capabilities, null, 2)))
+              document.getElementById("client-capabilities")
+                .appendChild(document.createTextNode(JSON.stringify(capabilities, null, 2)))
               
               const isPlatformAuthenticatorAvailable = await window.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable()
               console.log("isPlatformAuthenticatorAvailable", isPlatformAuthenticatorAvailable)
-              document.getElementById("platform").appendChild(document.createTextNode(isPlatformAuthenticatorAvailable.toString()))
+              document.getElementById("is-user-verifying-platform-authenticator-available")
+                .appendChild(document.createTextNode(isPlatformAuthenticatorAvailable.toString()))
             })()
           `}
         </Script>
